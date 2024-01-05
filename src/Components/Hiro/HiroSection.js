258415feeccb8075b12c40ca64d8b3499268929c
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2024-01-05 19:25:48 +0400

    Added logo section

--- src/Components/Hiro/HiroSection.js
+++ src/Components/Hiro/HiroSection.js
@@ -2,7 +2,7 @@ import "./Hiro.css"
 import Logo from "../Logo/Logo"
 
 const pic = require("./pic.PNG")
-const bosch = require("./Bosch-logo.jpeg")
+
 
 
 const HiroSection = () => {
@@ -26,12 +26,12 @@ const HiroSection = () => {
                         <p>
                         Ready to experience hassle-free appliance repairs? Schedule your service now and let our expert technicians bring your appliances back to life. Click below to book your appointment or contact us for immediate assistance.
                         </p>
-                        <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                            <a href="javascript:void(0)" className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
-                                Book Appointment
+                        <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0 text-lg font-bold">
+                            <a href="javascript:void(0)" className="block py-2 px-4 text-center text-white bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
+                                Calculate Estimate Repair Cost
                             </a>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
-                                Login
+                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
+                            Book Appointment
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
                                 </svg>
