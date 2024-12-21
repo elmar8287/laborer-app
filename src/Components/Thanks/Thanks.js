f0de48af7a9f8a9eaeccf3d05349c1f97d8921db
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2024-12-21 21:18:10 +0000

    From updated

--- src/Components/Thanks/Thanks.js
+++ src/Components/Thanks/Thanks.js
@@ -1,23 +1,34 @@
-const logo = require("../Navbar/alpha_logo.PNG")
+const logo = require("../Navbar/alpha_logo.PNG");
 
 export default () => {
-    return (
-        <section className="py-14">
-            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-                <div className="max-w-3xl mx-auto text-center">
-                    <h2 className="p-10 text-4xl sm:text-6xl font-bold mx-auto text-green-600">Congratulations!</h2>
-                    <figure>
-                        <blockquote>
-                            <p className="text-gray-800 text-md">
-                            You have booked an appointment and we will contact you shortly. You can also find out about the status of your application by calling the number
-                            </p>
-                        </blockquote>
-                        <div className="p-12">
-                        <p src={logo} className="text-4xl font-bold mx-auto">346-268-5088</p>
-                    </div>
-                    </figure>
-                </div>
+  return (
+    <section className="py-10 sm:py-14">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="max-w-3xl mx-auto text-center">
+          <h2 className="p-6 sm:p-10 text-3xl sm:text-5xl font-bold text-green-600">
+            Congratulations!
+          </h2>
+          <figure>
+            <blockquote>
+              <p className="text-gray-800 text-sm sm:text-base leading-relaxed">
+                You have booked an appointment and we will contact you shortly. 
+                You can also find out about the status of your application by calling the number:
+              </p>
+            </blockquote>
+            <div className="p-6 sm:p-12">
+              {/* Display the logo */}
+              <img
+                src={logo}
+                alt="Company Logo"
+                className="w-24 sm:w-32 h-auto mx-auto mb-4"
+              />
+              <p className="text-2xl sm:text-3xl font-bold text-gray-900">
+                346-268-5088
+              </p>
             </div>
-        </section>
-    )
-}
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
+};
